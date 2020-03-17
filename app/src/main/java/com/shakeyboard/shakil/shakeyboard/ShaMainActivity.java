6a83f7f359e0f5b467e8000c41155d0e27cb0e2e
shakiz <activities335@gmail.com>
2020-03-17 16:06:13 +0600

    project refactoring and main activity

---
+++ app/src/main/java/com/shakeyboard/shakil/shakeyboard/ShaMainActivity.java
@@ -0,0 +1,13 @@
+package com.shakeyboard.shakil.shakeyboard;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShaMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sha_main);
+    }
+}
